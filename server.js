e7b2fe27a1ddf6d8718493a4ebfc4d0cdcf802e1
Aaron-GF <aarongarcia567@gmail.com>
2025-06-09 14:19:31 +0200

    fix: correct bcrypt hash and compare implementation in server.js

--- server.js
+++ server.js
@@ -11,7 +11,13 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+    // $2b$12$i.CuxTtRifbw2/7qUcd7.ObOb2vFQ1o8vYH/KsQ4UB6Cy/1gFOAse
+    bcrypt.compare(someOtherPlaintextPassword, hash, (err, res) => {
+        console.log(res); // false
+    })
+});
 
 
 //END_ASYNC
